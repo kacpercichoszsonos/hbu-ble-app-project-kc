2b0114c4b31ca029fbb7d4e863c527be09415185
Kacper Cichosz <kacper.cichosz@sonos.com>
2022-12-15 17:35:43 +0000

    Get Gatt Server settings on launch

--- BLEProject/Duke Control/DukeControlViewController.swift
+++ BLEProject/Duke Control/DukeControlViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class DukeControlViewController: ModelledViewController<DukeControlViewModel> {
-    
+    @IBOutlet weak var ancSwitch: UISwitch!
+    @IBOutlet weak var headTrackingSwitch: UISwitch!
+    @IBOutlet weak var productNameTextfield: UITextField!
+    @IBOutlet weak var dukeConnectedView: UIView!
+    @IBOutlet weak var dukeNotConnectedView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
     }
-    
+
+    override func updateView(_ type: DukeControlViewModel.UpdateType) {
+        switch type {
+        case .dukeNotConnected:
+            self.dukeNotConnectedView.isHidden = false
+            self.dukeConnectedView.isHidden = true
+        case .reloadDukeValues:
+            self.productNameTextfield.placeholder = self.viewModel.dukeModel?.deviceName
+            if let ancMode = self.viewModel.dukeModel?.ancMode {
+                self.ancSwitch.isOn = ancMode
+            }
+            if let headTrackingMode = self.viewModel.dukeModel?.headTrackingMode {
+                self.headTrackingSwitch.isOn = headTrackingMode
+            }
+            self.dukeNotConnectedView.isHidden = true
+            self.dukeConnectedView.isHidden = false
+        }
+    }
+
+    @IBAction func productNameTextFieldDidEndOnExit(_ sender: UITextField) {
+        self.viewModel.writeData(setting: CurrentlyUsedSettings.deviceName, value: sender.text)
+    }
+
     @IBAction func playPauseBtnTapped(_ sender: Any) {
         //        BleManager.shared.writeData(data: Data[CommandType.COMMAND_TYPE_COMMAND])
     }
@@ -24,10 +51,6 @@ class DukeControlViewController: ModelledViewController<DukeControlViewModel> {
 
     @IBAction func headTrackingSwitchChanged(_ sender: UISwitch) {
         self.viewModel.writeData(setting: CurrentlyUsedSettings.headTracking, value: sender.isOn)
-    }
-
-    @IBAction func productNameTextFieldEditingDidEnd(_ sender: UITextField) {
-        self.viewModel.writeData(setting: CurrentlyUsedSettings.deviceName, value: sender.text)
     }
 }
 
